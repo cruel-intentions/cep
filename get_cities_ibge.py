b51a1fd120e88b7f5b55e1456601331af9a43c17
hugosenari <hugosenari@gmail.com>
2021-07-20 02:10:55 -0300

    chore: first commit

--- get_cities_ibge.py
+++ get_cities_ibge.py
@@ -1,7 +1,13 @@
 import json
+import urllib
+from urllib.parse import urlencode
+
+mkargs = lambda uf_id: urlencode({"localidades": f"n6[n3[{uf_id}]]"})
+mkurl = lambda uf_id: f'https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2020/variaveis/9324?{mkargs(uf_id)}'
+
 curl_cmd = lambda uf: f"""
     curl \\
-        'https://servicodados.ibge.gov.br/api/v1/localidades/estados/{uf["id"]}/municipios' \\
+       '{mkurl(uf["id"])}' \\
         -o ./raw/cities/{uf["sigla"]}_ibge.json"""
 
 ufs = lambda: json.load(open("./raw/uf_ibge.json"))
